be784eae4be989420b0ed15c98cbd5ab5e57b731
LuisDanilo <luis.danilo.95@hotmail.com>
2023-04-29 00:12:06 -0500

    Added path alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import {useState} from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '@/assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import '@/App.css'
 
 function App() {
     const [count, setCount] = useState(0)
 
     return (
         <>
-            <meta name="root" content="Pagina inicial de Motivy App"/>
             <div>
                 <a href="https://vitejs.dev" target="_blank">
                     <img src={viteLogo} className="logo" alt="Vite logo"/>
